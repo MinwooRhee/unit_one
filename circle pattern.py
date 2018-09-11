ceeaf8fba1f2de7807ad5e9d731b604ccdf91296
Min Woo <minwoo@iMac5.local>
2018-09-11 11:09:35 -0400

    I accomplished patterns

---
+++ circle pattern.py
@@ -0,0 +1,29 @@
+import turtle
+
+turtle.speed(100)
+
+turtle.right(30)
+
+def drawcircles():
+    for x in range(200):
+        turtle.circle(x)
+        turtle.left(1.8)
+
+
+turtle.pencolor("#ff80ff")
+
+drawcircles()
+
+turtle.left(120)
+
+turtle.pencolor("#5cd6d6")
+
+drawcircles()
+
+turtle.left(120)
+
+turtle.pencolor("#4d4dff")
+
+drawcircles()
+
+turtle.exitonclick()
