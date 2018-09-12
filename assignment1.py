001e5fc4e5ab6596eea196d4ae8a2b6a49d273f4
Min Woo <minwoo@iMac5.local>
2018-09-12 11:04:41 -0400

    I learned how to make functions, loops and different things I can do with turtle

--- assignment1.py
+++ assignment1.py
@@ -1,13 +1,24 @@
+# Minwoo Rhee
+# 9/12/18
+# 9/12/18 11:01 AM
+# assignment1
+# draws a pattern with three sets of circles growing bigger in different colors
+
 import turtle
 
 turtle.speed(100)
 
 turtle.right(30)
 
+
 def drawcircles():
     for x in range(200):
         turtle.circle(x)
         turtle.left(1.8)
+# This is a function that draws many circles. Each time they get bigger.
+# The loop is used 200 times and the circles change angles by 1.8 degrees each time.
+# Since 200 x 1.8 = 360, the turtle will come back to the angle it started when the loop ends
+
 
 turtle.pencolor("#ff80ff")
 
